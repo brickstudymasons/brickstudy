75d8701b0cbecb1b93bb750768654da428d144b1
Candace Moore <Candace.Moore@mydre.org>
2024-04-19 08:48:21 +0000

    dicom converter mr

--- dicom_converter.sh
+++ dicom_converter.sh
@@ -6,6 +6,11 @@ for f in $(find /z/MRIs -type d -iwholename '/*/*/*/*/*/*/*/*')
 do
   # make names based on $f
   out=/z/output/${f#"/z/MRIs/"}
-  mkdir -p "$out"
-  dcm2bids_helper -d $f -o "$out"
+  if [ ! -d "$out" ] ; then
+    echo Creating new directory: $out
+    mkdir -p "$out"
+    dcm2bids_helper -d $f -o "$out"
+  else
+    echo Found existing directory: $out
+  fi
 done
