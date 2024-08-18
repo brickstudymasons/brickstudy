85e4e82c031f3f48d1d407c1ade9eda502a07688
Candace Moore <doctormakeda@gmail.com>
2024-08-18 15:50:53 +0200

    rremove bad asl

--- paralell-oxfor_asl_anat.sh
+++
@@ -1,7 +0,0 @@
-for f in $(find /home/fsluser/pcasl_festival/json_holder_1/ -type f -name '*.nii.gz') ; do
-	out=pcasl_anat_output/$(basename $f)
-	anat=/home/fsluser/home_holder/$(basename $f)
-	mkdir $out
-	oxford_asl -i $f -o $out --fslanat $anat --tis=4.5 --bolus=3.5  --casl &
-done
-wait
